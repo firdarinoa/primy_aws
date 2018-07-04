3a63b869dd776702aabb7c23b025c437ce180a2b
FirdaRinoa <firdasahidi@hotmail.com>
2018-07-04 21:59:28 +0700

    Initial Commit

---
+++ Primy/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Primy
+//
+//  Created by Firda Rinoa Sahidi on 7/4/18.
+//  Copyright © 2018 Firda Rinoa Sahidi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
